a108662ef784da7682a460cd16292988b2b474eb
alex.li <alex.li@owin.corp>
2023-06-28 13:47:26 +0800

    change ip to domain

--- query_tools.py
+++ query_tools.py
@@ -15,8 +15,8 @@ def Get_Wking_UserCount():
         'Content-Type' : 'application/json',
         'Authorization' : 'Bearer glsa_qOHsDNPG4z8Hr87LlXgpBzQokb91s2Xn_79ffa6a3'   
     }
-    #response = session.get('https://wking-users.owin.info/api/User/onlineusers', headers = headers, verify=False)
-    response = session.get('http://192.168.82.160:30196/api/User/onlineusers', headers = headers, verify=False)
+    response = session.get('https://wking-users.owin.info/api/User/onlineusers', headers = headers, verify=False)
+    # response = session.get('http://192.168.82.160:30196/api/User/onlineusers', headers = headers, verify=False)
     user_count = json.loads(response.content).split()[-1]
     session.close()
     return user_count
