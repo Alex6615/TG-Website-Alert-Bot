807579bed7bc6cc3912b897bf3ece6e40303032c
root <root@ubuntu22.04-184-test>
2023-09-13 05:40:31 +0000

    repait setup.py

--- secrets/setup.py
+++ secrets/setup.py
@@ -4,4 +4,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
  
 setup(ext_modules=cythonize([f"{cwd}/secrets/secret_telegram.py"]))
+setup(ext_modules=cythonize([f"{cwd}/secrets/secret_account.py"]))
 setup(ext_modules=cythonize([f"{cwd}/secrets/targets.py"]))
